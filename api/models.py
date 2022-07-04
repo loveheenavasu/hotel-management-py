4f1872ae91baf3529f98961e0bb45685dcd572a0
loveheenavasu <trivediheenalove@gmail.com>
2022-07-04 18:33:34 +0530

    api done

--- api/models.py
+++ api/models.py
@@ -3,8 +3,8 @@ from django.db import models
 
 
 class Role(models.Model):
-    role_name = models.CharField(max_length=100)
-    permission = models.CharField(max_length=100)
+    role_name = models.CharField(max_length=200,null=True)
+    permission = models.CharField(max_length=200, null=True)
 
 
 class UserManager(BaseUserManager):
