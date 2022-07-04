1f2cb8bd22152188c964fd5e5a2fa2eec1c60977
loveheenavasu <trivediheenalove@gmail.com>
2022-07-04 17:22:22 +0530

    api done

--- api/models.py
+++ api/models.py
@@ -63,7 +63,7 @@ class User(AbstractBaseUser, PermissionsMixin):
                                    )
     date_joined = models.DateTimeField(auto_now_add=True)
     contact = models.CharField(max_length=15)
-    contact_is_verified = models.BooleanField(default=False)
+    # contact_is_verified = models.BooleanField(default=False)
     USERNAME_FIELD = 'email'
     EMAIL_FIELD = 'email'
     REQUIRED_FIELDS = []
