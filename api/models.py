134fb40b079b7e592f1bf017051c6a6c197db74b
lovet <rivediheenalove@gmail.com>
2022-07-12 11:26:11 +0530

    alter permission field into Array field in Role model.py

--- api/models.py
+++ api/models.py
@@ -1,10 +1,11 @@
 from django.contrib.auth.models import (BaseUserManager, AbstractBaseUser, PermissionsMixin, )
 from django.db import models
+from django.contrib.postgres.fields import ArrayField
 
 
 class Role(models.Model):
     role_name = models.CharField(max_length=100)
-    permission = models.CharField(max_length=100)
+    permission = ArrayField(models.CharField(max_length=100, null=True, blank=True), default=list)
 
 
 class UserManager(BaseUserManager):
